c89254e736b6e6e921cb586cf3904de4b6ae7466
Tony Chen <tchen51@stuy.edu>
2025-05-21 22:23:50 -0400

    merge ian's progress

--- rockstar.py
+++ rockstar.py
@@ -4,29 +4,30 @@ def preprocess(code):
 def get_word(code, index):
     code = code[index:]
     end = code.find(" ")
-    return code[index:end], end - index
+    return code[index:end]
 
 def generate_tokens(code):
     tokens = [] # example token {"action":"define var", "value":["cheese", 1]}
     i = 0
     while i < len(code):
-        word, len = get_word(code, i)
+        word= get_word(code, i)
         # if word == "something":
             # do something with potentially next word
 
 	# booleans
-	if word in ('true','right','ok','yes'):
-		tokens.append({"action":"boolean", "value":"true"})
-	if word in ('wrong','no','lies','false'):
-		tokens.append({"action":"boolean", "value":"false"})
-
+    if word in ('true','right','ok','yes'):
+        tokens.append({"action":"boolean", "value":"true"})
+        i += len(word)
+    if word in ('wrong','no','lies','false'):
+        tokens.append({"action":"boolean", "value":"false"})
+        i += len(word)
 	# functions
 
 
 	# null
-	if word in ("nothing", "nowhere", "nobody", "gone", "null"):
-		tokens.append({"action":"nullType", "value":"null"}) 
+    if word in ("nothing", "nowhere", "nobody", "gone", "null"):
+        tokens.append({"action":"nullType", "value":"null"}) 
+        i += len(word)
 
-        i += len + 1
 
     return tokens
